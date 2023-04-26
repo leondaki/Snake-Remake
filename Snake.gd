327b953d65341b4d2f27cfc919745458d2947863
YoopaYenkels <76530743+YoopaYenkels@users.noreply.github.com>
2023-04-26 00:42:40 -0400

    Implement main movement

--- Snake.gd
+++ Snake.gd
@@ -2,9 +2,9 @@ extends Area2D
 # Declare member variables here. Examples:
 # var a: int = 2
 # var b: String = "text"
-export var updateSpeed = 0.01
+export var updateSpeed = 0.02
 var moveCounter = updateSpeed
-export var moveAmount = 2
+export var moveAmount = 4
 
 var currentDir = Vector2.RIGHT
 var nextDir = Vector2.RIGHT
@@ -13,20 +13,45 @@ var lastSwitch = Vector2.ZERO
 var apple_scene = load("res://Apple.tscn")
 var snake_body_scene = load("res://snake_body.tscn")
 
+var snakeBodyParts
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	snakeBodyParts = get_node("/root/Main/SnakeBodyParts")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	SetNextDir()
 	
-	if (fmod(position.x - 32, 64) == 0 and abs(position.x - lastSwitch.x) >= 64) \
-		or (fmod(position.y - 32, 64) == 0 and abs(position.y - lastSwitch.y) >= 64):
-		lastSwitch.x = position.x
-		lastSwitch.y = position.y
-		currentDir = nextDir
-
+	# restricts movement to grid, must turn at least one box apart
+	if fmod(position.x, 64) == 0 and fmod(position.y, 64) == 0:
+						
+		for i in snakeBodyParts.get_child_count():
+			var bodyPart = snakeBodyParts.get_child(i)
+			var prevBodyPart = snakeBodyParts.get_child(i+1)
+			
+			if i == 0:	
+				if (nextDir != currentDir):
+					snakeBodyParts.get_child(1).destination.push_back(position)					
+					currentDir = nextDir	
+					snakeBodyParts.get_child(1).directions.push_back(currentDir)
+			
+			if i >= 1:
+				if !bodyPart.directions.empty():
+					
+					if bodyPart.position.x == bodyPart.destination[0].x and \
+					   bodyPart.position.y == bodyPart.destination[0].y:		
+						
+						bodyPart.currentDir = bodyPart.directions[0]
+					
+						bodyPart.destination.pop_front()		
+						bodyPart.directions.pop_front()	
+				
+				if i < snakeBodyParts.get_child_count()-1:	
+					if bodyPart.currentDir != prevBodyPart.currentDir:
+						prevBodyPart.destination.push_back(bodyPart.position)						
+						prevBodyPart.directions.push_back(bodyPart.currentDir)	
+											
 	match currentDir:
 		Vector2.UP:
 			rotation_degrees = -90
@@ -39,11 +64,13 @@ func _process(delta: float) -> void:
 			
 	if moveCounter <= 0:
 		moveCounter = updateSpeed
-		position += currentDir * moveAmount
-		print('x: ', position.x, ', y: ', position.y )
+		for i in snakeBodyParts.get_child_count():
+			snakeBodyParts.get_child(i).position += \
+				snakeBodyParts.get_child(i).currentDir * moveAmount
+				
 	moveCounter -= delta
 
-func SetNextDir() -> void: 
+func SetNextDir() -> void: 	
 	if Input.is_action_just_pressed("move_right") && currentDir != Vector2.LEFT:
 		nextDir = Vector2.RIGHT
 	if Input.is_action_just_pressed("move_left")  && currentDir != Vector2.RIGHT:
---
+++ snake_body.gd
@@ -0,0 +1,27 @@
+extends Area2D
+
+export var currentDir = Vector2.RIGHT
+var destination= []
+var directions= []
+
+# Declare member variables here. Examples:
+# var a: int = 2
+# var b: String = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	match currentDir:
+		Vector2.UP:
+			rotation_degrees = -90
+		Vector2.DOWN:
+			rotation_degrees = 90
+		Vector2.LEFT:
+			rotation_degrees = 180
+		Vector2.RIGHT:
+			rotation_degrees = 0
